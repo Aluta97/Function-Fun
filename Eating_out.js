f3a1a26a26626a5a124977d444e740e4b65ea693
Lusindiso <lusindiso@projectcodex.co>
2016-04-08 15:43:08 +0200

    initial commit

---
+++ Eating_out.js
@@ -0,0 +1,44 @@
+var team = "Your team ate 8 burgers, drank 17 beers and 11 fanta's";
+
+var team = team
+.replace("Your team ate ","")
+.replace(" and ",",")
+.replace(" drank ","")
+.split(",");
+    console.log(team);
+
+var celtic = team.map(function(t){
+      var parts = t.split(" ");
+        console.log(parts);
+  
+  return{
+    
+    food:parts[1],
+    quantity:Number(parts[0])
+    
+  };
+  
+});
+console.log(celtic);
+
+
+var data =  "burgers - 37 each, beers - 12, fanta's 9";
+
+var data = data 
+.replace(" each, ",",")
+.replace(/ -/g,"")
+.replace(", ",",")
+.split(",");
+console.log(data);
+
+newData = data.map(function(r){
+      var parts2 = r.split(" ");
+          console.log(parts2);
+ 
+      return{
+        junk:parts2[0],
+        price:Number(parts2[1])
+      };
+});
+
+    console.log(newData);
